17de4f8ee630b60d016e4c962f649676bc4d27c3
Laboralphy <raphael.marandet@gmail.com>
2017-12-13 22:36:48 +0100

    txat hot fix saussisson / simplification

--- public/scripts/src/components/chat-channels.vue
+++ public/scripts/src/components/chat-channels.vue
@@ -26,29 +26,6 @@
             }
         },
         methods: {
-        	add: function(id, sCaption) {
-        		if (this.find(id)) {
-        			throw new Error('tab id #' + id + ' is already defined');
-                }
-        		this.items.push({
-                    id: id,
-                    caption: STRINGS.chat.tabs[sCaption],
-                    selected: false,
-                    notified: false
-                });
-            },
-
-            find: function(id) {
-        		return this.items.find(i => i.id === id);
-            },
-
-            select: function(id, bSelected = true) {
-        	    this.items.forEach(t => t.selected = false);
-        	    let oTab = this.find(id);
-        	    oTab.selected = bSelected;
-        	    oTab.notified = false;
-            },
-
             clickHandler: function(item) {
                 this.$emit('select', item);
             }
