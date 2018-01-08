800eaeb4d7dfbd88ac4b3428b40496ff98b2c974
Laboralphy <raphael.marandet@gmail.com>
2018-01-08 23:44:04 +0100

    integration game chat

--- public/scripts/src/game/index.js
+++ public/scripts/src/game/index.js
@@ -3,10 +3,23 @@ import LEVEL_CITY from './levels/city.lvl.js';
 class Game extends O876_Raycaster.GameAbstract {
 	init() {
 		this.on('leveldata', function(wd) {
-			console.log(LEVEL_CITY);
 			wd.data = LEVEL_CITY;
 		});
+        this.on('key.down', event => this.gameEventKey(event));
 	}
+
+
+    /**
+     * Event triggered when a key is pressed
+     * @param oEvent {k : pressed key code }
+     */
+    gameEventKey(oEvent) {
+        switch (oEvent.k) {
+            case KEYS.ALPHANUM.ENTER:
+
+                break;
+        }
+    }
 }
 
 export default Game;
