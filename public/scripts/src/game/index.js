32f58eb4e32b0090d1d14bdcc780e7317d5c438d
Laboralphy <raphael.marandet@gmail.com>
2018-01-09 16:10:54 +0100

    changer config format

--- public/scripts/src/game/index.js
+++ public/scripts/src/game/index.js
@@ -5,7 +5,14 @@ class Game extends O876_Raycaster.GameAbstract {
 		this.on('leveldata', function(wd) {
 			wd.data = LEVEL_CITY;
 		});
-        this.on('key.down', event => this.gameEventKey(event));
+		this.on('key.down', event => this.gameEventKey(event));
+		this.on('enter', event => this.gameEventEnter(event));
+	}
+
+
+
+	gameEventEnter(oEvent) {
+
 	}
 
 
@@ -15,11 +22,43 @@ class Game extends O876_Raycaster.GameAbstract {
      */
     gameEventKey(oEvent) {
         switch (oEvent.k) {
-            case KEYS.ALPHANUM.ENTER:
+			case KEYS.ENTER:
 
-                break;
+				break;
+
+			case KEYS.ESCAPE:
+				this.showOverlay();
+				break;
         }
     }
+
+
+	/**
+	 * Renvoi l'instance du joueur local
+	 * @return O876_Raycaster.Mobile
+	 */
+	getPlayer() {
+    	return this.oRaycaster.oCamera;
+	}
+
+	/**
+	 * Affichage d'un overlay
+	 * Freeze les contrôles du joueur
+	 */
+	showOverlay() {
+		Main.screen.style.opacity = 0.5;
+		this.getPlayer().getThinker().freeze();
+	}
+
+	/**
+	 * Supprime l'overlay
+	 * Réactive les contrôles du joueur
+	 */
+	hideOverlay() {
+		Main.screen.style.opacity = 1;
+		this.getPlayer().getThinker().thaw();
+	}
+
 }
 
 export default Game;
